07e9222b098cf2fcb12b18303484e648719bb625
Waqas Ali <waqasansari4547@gmail.com>
2023-10-11 13:55:05 -0700

    Object

--- Assignment9Dom/script.js
+++ Assignment9Dom/script.js
@@ -1,67 +1,67 @@
-// function Dom(){
-//     var Num1 = parseInt(prompt('Enter 1st Number'))
-//     var Num2 = parseInt(prompt('Enter 2nd Number'))
-//     var Num3 = parseInt(prompt('Enter 3rd Number'))
-//     var Num4 = parseInt(prompt('Enter 4th Number'))
+function Dom(){
+    var Num1 = parseInt(prompt('Enter 1st Number'))
+    var Num2 = parseInt(prompt('Enter 2nd Number'))
+    var Num3 = parseInt(prompt('Enter 3rd Number'))
+    var Num4 = parseInt(prompt('Enter 4th Number'))
 
-//     var Max = Math.max(Num1, Num2, Num3, Num4)
-//     document.getElementById('second-div').innerHTML = "Your Maximun Number is" + Max 
-// }
-
-
-
-// function Dom(){
-
-// var ObtainMarks = parseInt(prompt("Enter your Obtain Marks"))
-// var TotalMarks = parseInt(prompt("Enter your Total Marks"))
-// var total = (ObtainMarks/TotalMarks)*100
-
-// document.getElementById('second-div').innerHTML = " Your Percentage is " + total 
-
-// }
+    var Max = Math.max(Num1, Num2, Num3, Num4)
+    document.getElementById('second-div').innerHTML = "Your Maximun Number is" + Max 
+}
 
 
 
+function Dom(){
 
-// function Dom(){
+var ObtainMarks = parseInt(prompt("Enter your Obtain Marks"))
+var TotalMarks = parseInt(prompt("Enter your Total Marks"))
+var total = (ObtainMarks/TotalMarks)*100
 
-// var a = parseInt(prompt("Enter your first number"))
-// var b = parseInt(prompt("Enter your second number"))
-// var Operater = prompt("Enter your operater")
+document.getElementById('second-div').innerHTML = " Your Percentage is " + total 
 
-//  switch (Operater){
-//     case '*': document.getElementById('second-div').innerHTML = " Your Answer is " + (a*b) 
-//     break;
-//     case '+': document.getElementById('second-div').innerHTML = " Your Answer is " + (a+b) 
-//     break;
-//     case '-': document.getElementById('second-div').innerHTML = " Your Answer is " + (a-b) 
-//     break;
-//     case '/': document.getElementById('second-div').innerHTML = " Your Answer is " + (a/b) 
-//     break;
-//     case '%': document.getElementById('second-div').innerHTML = " Your Answer is " + (a%b) 
-//     break;
-//     default: document.getElementById('second-div').innerHTML = " You Select wrong Opeerator "
-//  } 
-// }
+}
 
 
 
 
-// function Dom(){
+function Dom(){
 
-//  var a = parseInt(prompt('Enter a number'))
+var a = parseInt(prompt("Enter your first number"))
+var b = parseInt(prompt("Enter your second number"))
+var Operater = prompt("Enter your operater")
 
-//   if(a > 0){
-//      document.getElementById('second-div').innerHTML = " Your Percentage is Positive" 
+ switch (Operater){
+    case '*': document.getElementById('second-div').innerHTML = " Your Answer is " + (a*b) 
+    break;
+    case '+': document.getElementById('second-div').innerHTML = " Your Answer is " + (a+b) 
+    break;
+    case '-': document.getElementById('second-div').innerHTML = " Your Answer is " + (a-b) 
+    break;
+    case '/': document.getElementById('second-div').innerHTML = " Your Answer is " + (a/b) 
+    break;
+    case '%': document.getElementById('second-div').innerHTML = " Your Answer is " + (a%b) 
+    break;
+    default: document.getElementById('second-div').innerHTML = " You Select wrong Opeerator "
+ } 
+}
 
-//   }
-//   else if(a<0){
-//      document.getElementById('second-div').innerHTML = " Your Percentage is Negative" 
 
-//   }
-//   else {
-//     document.getElementById('second-div').innerHTML = " Your Percentage is Zero or Null" 
 
-//   }
 
-// }
+function Dom(){
+
+ var a = parseInt(prompt('Enter a number'))
+
+  if(a > 0){
+     document.getElementById('second-div').innerHTML = " Your Percentage is Positive" 
+
+  }
+  else if(a<0){
+     document.getElementById('second-div').innerHTML = " Your Percentage is Negative" 
+
+  }
+  else {
+    document.getElementById('second-div').innerHTML = " Your Percentage is Zero or Null" 
+
+  }
+
+}
---
+++ Leture Object/object.js
@@ -0,0 +1,38 @@
+var Names = {
+    name : "asad",
+    Class : "6th",
+    Rollnumber : 12234,
+    Batch : "B",
+    Array1 : [1, 2, 3, 4, 5]
+}
+// console.log("tHE VALUE OF ",Names.Class)
+console.log("tHE VALUE OF ",Names.Array1[0])
+
+
+
+var NewArray = [Names,
+    Class1 = {
+        Totalstd : 23,
+        TeacherName : "Ali",
+        Salary : 20000
+    },
+    Class2 = {
+        Totalstd : 53,
+        TeacherName : "Zain",
+        Salary : 25000
+    },
+    Class3 = {
+        Totalstd : 41,
+        TeacherName : "Sikandar",
+        Salary : 30000
+    }
+    // First1: Objective() {
+    //     console.log("I hope this will be interesting topic")
+    //     return "hello"
+    // }
+]
+
+// Names.Rollnumber = 21319;
+// console.log(NewArray[2].Totalstd)
+// console.log(NewArray[0].Rollnumber)
+// console.log(NewArray[3].Objective())
